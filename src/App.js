af10f7fe310d271550116382172985c84cb7ab3a
Linda Mazibuko <MZBLIN006@myuct.ac.za>
2022-08-29 18:12:41 +0200

    Delete from UI using filter function(JavaScript)

--- src/App.js
+++ src/App.js
@@ -24,9 +24,11 @@ function App() {
         reminder:true,
     }
 ])
+
 //Deleete task
 const deleteTask=(id)=>{
   console.log('delete',id)
+  setTasks(tasks.filter((task)=>task.id!==id))
 }
 
   return (
