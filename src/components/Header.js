5c82ed96b3d8a691d8a5802c44029560b3be201b
Linda Mazibuko <MZBLIN006@myuct.ac.za>
2022-08-29 08:43:38 +0200

    inline style

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types'
 const Header = ({title}) => {
   return (
     <header>
-        <h1>{title}</h1>
+        <h1 style={{color:'red',backgroundColor:"black"}}>{title}</h1>
     </header>
   )
 }
 
-/*Header.defaultProps={
+Header.defaultProps={
     title: 'Task Tracker'
-}*/
+}
 
 Header.propTypes={
     title:PropTypes.string.isRequired
