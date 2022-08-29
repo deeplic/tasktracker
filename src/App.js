94b42a6f715000889ce6cc9b6a2e03ca72f17cb6
Linda Mazibuko <MZBLIN006@myuct.ac.za>
2022-08-29 08:12:30 +0200

    HeaderC (created with rafce) added to AppC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import Header from "./components/Header";
 
 function App() {
   //can write javascript here
   return (
     <div className="container">
-      <h1>Hello from react</h1>
+      <Header></Header>
     </div>
   );
 }
